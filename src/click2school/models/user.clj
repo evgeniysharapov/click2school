a01d04e1513625995cdae5016e447f367bcf59d0
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-03-29 17:49:39 +0600

    Renamed methods to be more consistent.

--- src/click2school/models/user.clj
+++ src/click2school/models/user.clj
@@ -1,9 +1,9 @@
 (ns  click2school.models.user
-  (:require [noir.session :as session]
-            [noir.util.crypt :as crypt]
+  (:require [click2school.utils :as utils]
+  			[noir.util.crypt :as crypt]
             [noir.validation :as validate]))
 
-(def *data*
+(def- *data*
   (ref
    [{:id 1,
      :first-name "Evgeniy",
@@ -13,24 +13,22 @@
      :gender "Male",
      :email "evgeniy.sharapove@gmail.com"}
 
-    {:id 2, :first-name "Chittu", :last-name "Desai", :gender "Male", :username "test2", :password "test2", :email "chittu_d@yahoo.com"
-     }
+    {:id 2, :first-name "Chittu", :last-name "Desai", :gender "Male", :username "test2", :password "test2", :email "chittu_d@yahoo.com"}
     {:id 3, :first-name "Darla", :last-name "Sparrow", :gender "Female", :username "test3", :password "test3", :email "test@gmail.com"}
     {:id 4, :first-name "Roger", :last-name "Ellis", :gender "Male", :username "test4", :password "test4", :email "roger.ellis@emai.com"}]))
 
 
-(defn fetch-list []
+(defn get-list []
+"Returns list of all users"
   @*data*)
 
-
-(defn fetch [id]
+(defn find-by-id [id]
   (first
    (filter #(= id (:id %)) (fetch-list))))
 
-(defn find-by-full-name [{ first-name :first-name, last-name :last-name}]
+(defn find-by-first-last-name [{ first-name :first-name, last-name :last-name}]
   (first
    (filter #(and (= first-name (:first-name %)) (= last-name (:last-name %))) (fetch-list))))
-
 
 (defn find-by-username [{username :username}]
   (first
