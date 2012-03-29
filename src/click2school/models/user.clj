1e9361bf316c0d6e98866f73963ef34a62bc0b19
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-03-30 04:46:37 +0600

    Lined up default users list.

--- src/click2school/models/user.clj
+++ src/click2school/models/user.clj
@@ -7,16 +7,44 @@
   (ref
    [{:id 1,
      :first-name "Evgeniy",
+     :middle-name "N."
      :last-name "Sharapov",
      :username "test",
      :password "test",
      :gender "Male",
-     :email "evgeniy.sharapove@gmail.com"}
+     :email "evgeniy.sharapove@gmail.com",
+     :org "My Organization",
+     :roles [:admin]}
 
-    {:id 2, :first-name "Chittu", :last-name "Desai", :gender "Male", :username "test2", :password "test2", :email "chittu_d@yahoo.com"}
-    {:id 3, :first-name "Darla", :last-name "Sparrow", :gender "Female", :username "test3", :password "test3", :email "test@gmail.com"}
-    {:id 4, :first-name "Roger", :last-name "Ellis", :gender "Male", :username "test4", :password "test4", :email "roger.ellis@emai.com"}]))
-
+    {:id 2,
+     :first-name "Chittu",
+     :last-name "Desai",
+     :gender "Male",
+     :username "test2",
+     :password "test2",
+     :email "chittu_d@yahoo.com",
+     :org "Siemens",
+     :roles [:parent]}
+    
+    {:id 3,
+     :first-name "Darla",
+     :last-name "Sparrow",
+     :gender "Female",
+     :username "test3",
+     :password "test3",
+     :email "test@gmail.com",
+     :org "John Hopkins Elementary School",
+     :roles [:teacher]}
+    
+    {:id 4,
+     :first-name "Roger",
+     :last-name "Ellis",
+     :gender "Male",
+     :username "test4",
+     :password "test4",
+     :email "roger.ellis@emai.com",
+     :org "Siemens",
+     :roles [:parent]}]))
 
 (defn get-list []
 "Returns list of all users"
