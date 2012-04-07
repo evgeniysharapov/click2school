d42a0cff2e5a4e4bf9fec78e812c5ea53cd2a3c3
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-07 13:00:29 +0600

    Added tables data population from the YAML file.

--- src/lobos/migrations.clj
+++ src/lobos/migrations.clj
@@ -1,7 +1,12 @@
 (ns lobos.migrations
   (:refer-clojure :exclude [alter drop bigint boolean char double float time])
   (:use (lobos [migration :only [defmigration]]
-               core schema config helpers)))
+               core schema config helpers)
+        (korma
+         [db :only [defdb]]
+         [core :only [insert values defentity]]))
+  (:require [clojure.java.jdbc :as sql]
+            [clj-yaml.core :as yaml]))
 
 (defmacro def-new-table [tname & ddl]
   `(defmigration ~(symbol (str "add-" (name  tname) "-table"))
@@ -20,6 +25,7 @@
   (varchar :middle_name 100)
   (varchar :last_name 100)
   (varchar :org 100)
+  (varchar :email 100)
   (boolean :gender))
 
 (def-new-table :users
@@ -30,6 +36,18 @@
   (timestamp :last_logged_in :null)
   (check :name (> (length :name) 1))
   (check :name (> (length :password) 1)))
+
+(def-new-table :user_groups
+  (varchar :name 100)
+  (refer-to :users))
+
+(defmigration add-user-groups-link-table
+  (up [] (create (table :user_group_links
+                        (integer :user_id [:refer :users :id :on-delete :set-null])
+                        (integer :user_group_id [:refer :user_groups :id :on-delete :set-null])
+                        (timestamps)
+                        (primary-key [:user_id :user_group_id]))))
+  (down [] (drop (table :user_group_links))))
 
 (def-new-table :messages
   (varchar :subject 200)
@@ -60,3 +78,17 @@
 (def-new-table :asgners
   (refer-to :asgnmts)
   (refer-to :users))
+
+;;; Insert some test data
+(defmigration add-basic-data
+  (up [] (do
+           (defentity roles)
+           (insert roles (values (:roles (yaml/parse-string (slurp "./resources/fixture.yml")))))
+           (defentity persons)
+           (insert persons (values (:persons (yaml/parse-string (slurp "./resources/fixture.yml")))))
+           (defentity users)
+           (insert users (values (:users (yaml/parse-string (slurp "./resources/fixture.yml")))))
+           (defentity messages)
+           (insert messages (values (:messages (yaml/parse-string (slurp "./resources/fixture.yml")))))
+           ))
+  (down [] (print "delete data")))
