1359646ffbedf46d8b51d4579d4d251fc6a6bcf0
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-08 21:41:31 +0600

    added fuzzy search for a person by last name and first name. added fullname function.

--- src/click2school/models/person.clj
+++ src/click2school/models/person.clj
@@ -1,7 +1,19 @@
 (ns click2school.models.person
-  (:require [clj-record.boot])
+  (:require [clj-record.boot]
+            [clj-record.query :as query])
   (:use [click2school.config.db]))
 
 (clj-record.core/init-model
  (:associations
   (has-many user)))
+
+(defn fuzzy-find
+  "Fuzzily searches for a person looking for a subtring Q in last name and first name"
+  [q]
+  (let [like-q (.toUpperCase (str "%" q "%"))]
+    (find-by-sql ["select * from persons where upper(first_name) like ? or upper(last_name) like ? ", like-q, like-q])))
+
+(defn fullname
+  "Returns full name of the person P."
+  [p]
+  (str (:first_name p) " " (:last_name p)))
