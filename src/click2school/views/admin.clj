9843f235d1c635602feff0f6a778e36274408512
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-10 01:36:31 +0600

    Removed unused views.

--- src/click2school/views/admin.clj
+++
@@ -1,8 +0,0 @@
-(ns click2school.views.admin
-  (:require [click2school.views.common :as common])
-  (:use [noir.core :only [defpartial defpage]]))
-;;; 
-;;; This is an admin view. One that can add/delete/modify users,
-;;; assign classes, techers; upload forms and so on.
-;;; 
-
