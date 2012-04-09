d5d3f0b2e22f1b6a07330f32c0dd4ec2204ccf69
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-10 01:34:14 +0600

    person and user model pumped up, so they both can find each other.

--- src/click2school/models/person.clj
+++ src/click2school/models/person.clj
@@ -5,7 +5,7 @@
 
 (clj-record.core/init-model
  (:associations
-  (has-many user)))
+  (has-many user :model user)))
 
 (defn fuzzy-find
   "Fuzzily searches for a person looking for a subtring Q in last name and first name"
@@ -17,3 +17,14 @@
   "Returns full name of the person P."
   [p]
   (str (:first_name p) " " (:last_name p)))
+
+(defn find-person-by-fullname
+  [fullname]
+  (let [names (clojure.string/split fullname #" +")
+        fname (first names)
+        lname (last names)]
+    (find-record {:first_name fname, :last_name lname})))
+
+(find-person-by-fullname "Evgeniy N. Sharapov")
+
+
--- src/click2school/models/user.clj
+++ src/click2school/models/user.clj
@@ -6,4 +6,12 @@
 (clj-record.core/init-model
  (:associations
   (belongs-to person :model person)
-  (belongs-to role :model role)))
+  (belongs-to role :model role)
+  (has-many messages-to :model message :fk to_user_id)
+  (has-many messages-from :model message :fk from_user_id)))
+
+
+;(clj-record.core/find-records "message" {:to_user_id ((get-record 1) :id)})
+;(find-messages-to (get-record 1))
+
+
