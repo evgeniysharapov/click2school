e206df4a914f58834a1e91742db5820c6239dceb
unknown <sharapove@sharapove.prognoz.ru>
2012-04-11 12:35:22 +0600

    Made sidebar astructure of nested vectors. Removed superfluous comments.

--- src/click2school/views/sidebar.clj
+++ src/click2school/views/sidebar.clj
@@ -8,40 +8,29 @@
 ;;;
 ;;; This is an example of our sidebar structure
 ;;;
-
-;; (def sb
-;;  `("Communication"
-;;    (:messages "/messages" "Messages" :inbox)
-;;    (:emails "/emails" "Emails" :email)
-;;    "Calendar"
-;;    "Administration"
-;;    (:classes "/classes"  "Classes" :folder-close)
-;;    (:students "/students" "Students" :leaf)))
-
 (def *default-sidebar*
-  '("Communication"
-    (:message "/messages" "Messages" :inbox :active)
-    (:instants "/instants" "Instant Messages" :comment)
-    "Calendar"
-    (:meetings "/meetings" "Meetings" :user)
-    (:papers "/papers" "Papers Due" :file)
-    "Quizzes"
-    (:quizz1 "/quizzes/1" "Quizz 1" :star)
-    (:quizz2 "/quizzes/2" "Quizz 2" :empty)
-    (:quizz3 "/quizzes/3" "Quizz 3" :heart)
-    "Administration"
-    (:students "/students" "Students" :folder-close)
-    (:classes "/classes" "Classes" :leaf)
-    (:groups "/groups" "Groups" :leaf)
-    ))
+  [["Communication"
+    [:message "/messages" "Messages" :inbox :active]
+    [:instants "/instants" "Instant Messages" :comment]]
+   ["Calendar"
+    [:meetings  "/meetings"  "Meetings"  :user ]
+    [:papers  "/papers"  "Papers Due"  :file ]]
+   ["Quizzes"
+    [:quizz1  "/quizz/1"  "Quizz 1"  :star ]
+    [:quizz2  "/quizz/2"  "Quizz 2"  :empty ]
+    [:quizz3  "/quizz/3"  "Quizz 3"  :heart ]]
+   ["Administration"
+    [:students  "/students"  "Students"  :folder-close ]
+    [:classes  "/classes"  "Classes"  :leaf ]
+    [:groups  "/groups"  "Groups"  :leaf ]]]
+  )
 
 (defn item
   "Creates item on the sidebar. REST contains keyword :active added to the sidebar if the sidebar item should be active and a keyword for the the icon"
   [url title & more ]
-  (let [
-        css-classes (->> (->>  more (filter #(= :active %))) (apply kw->str))
+  (let [css-classes (->> (->>  more (filter #(= :active %))) (apply kw->str))
         icon (->  (->> more (filter #(not= :active %))) first)]
-    [:li {:class "active"}
+    [:li {:class css-classes}
      [:a {:shape "rect", :href url}
       [:i {:class (when icon (icon BOOTSTRAP-ICONS))}] title]]))
 
@@ -56,11 +45,12 @@
 (defn build-sidebar
   [bar]
   (sidebar
-   (for [v bar]
-     (cond  (string? v) (section v)
-            (seq? v) (item (second v) (nth v 2) (nth v 3))))))
+   (for [sec bar]
+       (do
+        (section (first sec))
+        (map #(apply item (rest %)) (rest  sec))))))
 
 (defn- find-sb-item-by-key
   "Returns a sidebar item by its key"
   [k sb]
-  (first (filter #(and (list? %) (= :classes (first %))) sb)))
+  (first (filter #(and (seq? %) (= :classes (first %))) sb)))
