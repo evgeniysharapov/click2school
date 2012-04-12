e1b3a967dd1dba1fde8fb279f9d1d2ebf528a761
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-13 01:42:37 +0600

    Added a data link from the form to the form_question.

--- src/click2school/models/form.clj
+++ src/click2school/models/form.clj
@@ -4,6 +4,7 @@
 
 (clj-record.core/init-model
  (:associations
-  (belongs-to user :model user :fk composer_user_id)))
+  (belongs-to user :model user :fk composer_user_id)
+  (has-many form-question :model form_question :fk form_id)))
 
 (create {:title "Super Form" :description "This is super puper form" :composer_user_id 1})
