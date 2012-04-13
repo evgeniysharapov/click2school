ceb862a3f8e9ae37374b82d02a62ec67faa7a612
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-13 06:40:50 +0600

    Added migration script that could be run on Heroku.

---
+++ src/click2school/config/migration.clj
@@ -0,0 +1,8 @@
+(ns click2school.config.migration
+  (:require [lobos.config]
+            [lobos.core]))
+
+(defn -main []
+  (print "Migrating database...")
+  (lobos.core/migrate)
+  (println " done"))
