860f61e28551aedeeba4babf58af43f5429c563e
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-18 16:33:36 +0600

    Added defpartials for form control elements.

--- src/click2school/views/questions.clj
+++ src/click2school/views/questions.clj
@@ -52,7 +52,63 @@
   :questions
   (list-of-questions-and-answers))
 
+(defpartial on-form-control
+  [type name label & [value placeholder]]
+  (let [ctrl-id (gensym name)
+        type-name (clojure.core/name type)]
+    [:div.control-group
+     [:label.control-label {:for ctrl-id} label]
+     [:div.controls
+      [(keyword (str (if (= type-name "textarea") type-name "input") ".input-xlarge"))
+       (merge  {:id ctrl-id :type type-name :name name}
+               (when value {:value value})
+               (when placeholder {:placeholder placeholder}))]
+      ]]))
+
+(defpartial text
+  [name label]
+  (on-form-control "text" name label))
+
+(defpartial text-area
+  [name label]
+  (on-form-control "textarea" name label))
+
+(defpartial checkbox
+  [name label]
+  (on-form-control "checkbox" name label))
+
+(defpartial checkbox-group
+  [name label]
+  (on-form-control "checkbox" name label))
+
+(defpartial radio
+  [name label]
+  (on-form-control "radio" name label))
+
+(defpartial radio-group
+  [name & val-label-map]
+  (for [[val lable] (apply hash-map val-label-map)]
+    (on-form-control "radio" name label val)))
+
+(defpartial question-edit-form [new-or-edit-title]
+  [:section#list-of-questions-and-answers
+   [:div.page-header
+    [:h1 new-or-edit-title]]
+   [:p "Type in the question, choose it's type and depending on its type you can choose answers. There are different types of questions. Depending on its type there are different options for adding answers."
+    [:dl.dl-horizontal
+     [:dt " Choice"]
+     [:dd "This is a quesion with mupltiple answer options. Only one option is correct. Good fit for quizzes. Also good for use in forms and surveys. Example, \" how old are you ?\" with options for different answers, only one of which would be correct."]
+     [:dt " Text"]
+     [:dd "This question assumes a freeform typing in response. Typical question could be \"How did you spend your summer\"."]
+     [:dt " Checkbox"]
+     [:dd "This question makes people to check the box in response. Example, \"I agree with terms and conditions\"."]
+     [:dt "Multiple Choice"]
+     [:dd "This questions are akin to " [:i "Choice"] " questions, but they let respondent to choose several answers."]]
+    [:form
+     [:fieldset
+      ]]
+    ]])
+
 (defview questions-create "/questions/create" []
   :questions
-  [:section#create-new-questions
-   [:div.page-header [:h1 "Create New Question"]]])
+  (question-edit-form "Create New Question"))
