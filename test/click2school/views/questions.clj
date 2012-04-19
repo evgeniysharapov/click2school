52b1eafe53018d3dc60501ff363a88724f38f577
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-20 05:52:26 +0600

    Removed messed up namespace.

--- test/click2school/views/questions.clj
+++
@@ -1,8 +0,0 @@
-(ns click2school.test.views.questions
-  (:use [click2school.views.questions] :reload)
-  (:use [clojure.test]
-        [noir.util.test]))
-
-
-(deftest test-controls
-  (is (re-matches  #"<div class=\"control-group\"><label class=\"control-label\" for=\"name.+\">label</label><div class=\"controls\"><input class=\"input-xlarge\" id=\"name.+\" name=\"name\" type=\"text\".*/></div></div>" (on-form-control "text" "name" "label"))))
