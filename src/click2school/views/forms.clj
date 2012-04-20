8268c50bc0d55983307a1caefb2d37731658d657
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2012-04-20 06:22:35 +0600

    Fixed updating form method.

--- src/click2school/views/forms.clj
+++ src/click2school/views/forms.clj
@@ -134,8 +134,8 @@
     (resp/redirect (url-for forms-edit {:id (:id f)}))
     ))
 
-(defpage forms-update [:post "/forms/update"] {:keys [id title desription composer_user_id] :as frm}
-  (form/update frm)
+(defpage forms-update [:post "/forms/update"] {:keys [id title description composer_user_id] :as frm}
+  (form/update {:id (Integer/parseInt id) :title title :description description :composer_user_id (Integer/parseInt composer_user_id)})
   (resp/redirect (url-for forms-view {:id id}))
   )
 
